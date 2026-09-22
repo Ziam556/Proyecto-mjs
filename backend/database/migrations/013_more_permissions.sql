1071a4cd623e0ccf3d371befe93b6124db7023a7
Ziam556 <sc876858@gmail.com>
2026-09-22 17:44:57 -0500

    feat: error en migracion

--- backend/database/migrations/013_more_permissions.sql
+++ backend/database/migrations/013_more_permissions.sql
@@ -1,6 +1,6 @@
 INSERT INTO permissions(permissions_name, permissions_codename)
 VALUES
 ('Visualizar usuarios','view_users'),
-('Habilitar usuarios', 'enable_user');
+('Habilitar usuarios', 'enable_user'),
 ('Deshabilitar usuarios', 'desabled_user'),
 ('Reportar usuarios', 'report_user');
